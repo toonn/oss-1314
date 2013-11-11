10789ddd4aaef01e8dcf022c6b4c88dc162f1d76
Joren Verspeurt <joren.verspeurt@student.kuleuven.be>
2013-11-11 18:20:38 +0100

    WIP Added CodeChangeCollector

---
+++ junit-r4.11/src/main/java/kuleuven/group6/collectors/CodeChangeCollector.java
@@ -0,0 +1,35 @@
+package kuleuven.group6.collectors;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchService;
+import java.util.HashSet;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+import jdepend.framework.*;
+import kuleuven.group6.TestCollectionInfo;
+
+public class CodeChangeCollector extends DataCollector<String> {
+
+	@Override
+	public void startCollecting(TestCollectionInfo testCollectionInfo) {
+		try {
+			WatchService watchService = FileSystems.getDefault().newWatchService();
+			Path codePath = FileSystems.getDefault().getPath(testCollectionInfo.getCodeDirectory().getPath(),(String[]) null);
+			codePath.register(watchService,ENTRY_MODIFY);
+			JDepend jDepend = new JDepend();
+			jDepend.addDirectory(testCollectionInfo.getTestDirectory().getPath());
+			HashSet<JavaClass> classSet = new HashSet<JavaClass>(jDepend.analyze());
+			//TODO something like this:
+			for(JavaClass jc : classSet){
+				jc.getImportedPackages();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
