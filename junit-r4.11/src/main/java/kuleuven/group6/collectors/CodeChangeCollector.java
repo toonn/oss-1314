2232b639d208774f65dcfd18a66033c114e86198
Joren Verspeurt <joren.verspeurt@student.kuleuven.be>
2013-11-16 19:32:53 +0100

    Added thread in CodeChangeCollector

--- junit-r4.11/src/main/java/kuleuven/group6/collectors/CodeChangeCollector.java
+++ junit-r4.11/src/main/java/kuleuven/group6/collectors/CodeChangeCollector.java
@@ -3,6 +3,7 @@ package kuleuven.group6.collectors;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Collection;
 import java.util.HashSet;
@@ -17,29 +18,72 @@ import kuleuven.group6.TestCollectionInfo;
  *
  */
 public class CodeChangeCollector extends DataCollector<String> {
+	protected HashSet<JavaClass> importedClasses;
+	protected WatchService watchService;
+	protected WatchKey codeKey;
+	protected WatchKey testKey;
 
+	/**
+	 * Calling this method will start the collection of code changes.
+	 * Every time it is called all test classes' dependencies are checked again.
+	 */
 	@Override
 	public void startCollecting(TestCollectionInfo testCollectionInfo) {
 		try {
-			WatchService watchService = FileSystems.getDefault().newWatchService();
-			Path codePath = FileSystems.getDefault().getPath(testCollectionInfo.getSourceDirectory().getPath(),(String[]) null);
-			codePath.register(watchService,ENTRY_MODIFY);
-			JDepend jDepend = new JDepend();
-			jDepend.addDirectory(testCollectionInfo.getTestDirectory().getPath());
-			HashSet<JavaClass> classSet = new HashSet<JavaClass>(jDepend.analyze());
-			HashSet<JavaClass> importedClasses = new HashSet<JavaClass>();
-			for(JavaClass jc : classSet){
-				HashSet<JavaPackage> ic = new HashSet<JavaPackage>((Collection<JavaPackage>)jc.getImportedPackages());
-				for(JavaPackage jp : ic){
-					importedClasses.addAll((Collection<JavaClass>)jp.getClasses());
-				}
-			}
-
-			
+			//Initiate the WatchService and WatchKeys by registering the test and code paths
+			registerPaths(testCollectionInfo);
+			//Extract dependencies using JDepend
+			extractDependencies(testCollectionInfo);
+			//Start watching directories
+			//TODO
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+	}
+
+	private class CodeChangeWatchThread extends Thread {
+		private HashSet<JavaClass> importedClasses;
+		private WatchService watchService;
+		private WatchKey codeKey;
+		private WatchKey testKey;
+
+		public CodeChangeWatchThread(HashSet<JavaClass> importedClasses, WatchService watchService, WatchKey codeKey, WatchKey testKey) {
+			this.importedClasses = importedClasses;
+			this.watchService = watchService;
+			this.codeKey = codeKey;
+			this.testKey = testKey;
+		}
+		
+		public void run() {
+			for(;;){
+				//TODO
+			}
+		}
+		
+	}
+	
+	protected void extractDependencies(TestCollectionInfo testCollectionInfo)
+			throws IOException {
+		JDepend jDepend = new JDepend();
+		jDepend.addDirectory(testCollectionInfo.getTestDirectory().getPath());
+		HashSet<JavaClass> classSet = new HashSet<JavaClass>(jDepend.analyze());
+		importedClasses = new HashSet<JavaClass>();
+		for(JavaClass jc : classSet){
+			HashSet<JavaPackage> ic = new HashSet<JavaPackage>((Collection<JavaPackage>)jc.getImportedPackages());
+			for(JavaPackage jp : ic){
+				importedClasses.addAll((Collection<JavaClass>)jp.getClasses());
+			}
+		}
+	}
+
+	protected void registerPaths(TestCollectionInfo testCollectionInfo)
+			throws IOException {
+		watchService = FileSystems.getDefault().newWatchService();
+		Path codePath = FileSystems.getDefault().getPath(testCollectionInfo.getSourceDirectory().getPath(),(String[]) null);
+		codeKey = codePath.register(watchService,ENTRY_MODIFY);
+		Path testPath = FileSystems.getDefault().getPath(testCollectionInfo.getSourceDirectory().getPath(), (String[]) null);
+		testKey = testPath.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 	}
 
 	@Override
