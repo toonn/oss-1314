d97158583b92aa1ca1caeea60deefd0429d229e1
Stef Noten <stef.noten@gmail.com>
2013-11-18 22:05:21 +0100

    LastFailureStatistic implementatie

--- junit-r4.11/src/main/java/kuleuven/group6/statistics/LastFailureStatistic.java
+++ junit-r4.11/src/main/java/kuleuven/group6/statistics/LastFailureStatistic.java
@@ -1,10 +1,11 @@
 package kuleuven.group6.statistics;
 
+import java.util.Date;
+
 import kuleuven.group6.collectors.DataCollectedListener;
 import kuleuven.group6.collectors.IDataCollectorManager;
 import kuleuven.group6.testcharacteristics.LastFailureDate;
 import kuleuven.group6.testcharacteristics.TestFailure;
-
 import org.junit.runner.Description;
 
 public class LastFailureStatistic<TestStatisticT> extends Statistic<LastFailureDate> {
@@ -24,18 +25,34 @@ public class LastFailureStatistic<TestStatisticT> extends Statistic<LastFailureD
 
 	@Override
 	protected LastFailureDate composeTestStatistic(Description description) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean hasLastFailureDate = false;
+		Date lastFailureDate = null;
+		
+		for (Description childDescription : description.getChildren()) {
+			LastFailureDate childData = getTestStatistic(childDescription);
+			if (childData == null)
+				continue;
+			
+			Date childLastFailureDate = childData.getLastFailureDate();
+			if (!hasLastFailureDate || childLastFailureDate.after(lastFailureDate)) {
+				lastFailureDate = childLastFailureDate;
+				hasLastFailureDate = true;
+			}
+		}
+		
+		if (!hasLastFailureDate)
+			return null;
+		
+		return new LastFailureDate(description, lastFailureDate);
 	}
 
 	@Override
 	protected LastFailureDate getDefaultTestStatistic(Description description) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	protected LastFailureDate calculateStatistic(TestFailure data) {
-		return null;
+		return new LastFailureDate(data.getTestDescription(), new Date());
 	}
 
 }
