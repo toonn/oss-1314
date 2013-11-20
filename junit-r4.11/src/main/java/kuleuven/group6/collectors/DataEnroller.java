dac9d22f1a6b921c0e13478540f17a90d0194c7a
Stef Noten <stef.noten@gmail.com>
2013-11-20 14:50:30 +0100

    DataEnroller.createConfiguredDataEnroller

--- junit-r4.11/src/main/java/kuleuven/group6/collectors/DataEnroller.java
+++ junit-r4.11/src/main/java/kuleuven/group6/collectors/DataEnroller.java
@@ -1,11 +1,16 @@
 package kuleuven.group6.collectors;
 
+import java.io.File;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import kuleuven.group6.RunNotificationSubscriber;
+import kuleuven.group6.testcharacteristics.CodeChange;
 import kuleuven.group6.testcharacteristics.ITestData;
+import kuleuven.group6.testcharacteristics.MethodCalls;
 import kuleuven.group6.testcharacteristics.TestFailure;
+import org.junit.runner.Description;
 
 /**
  * 
@@ -16,9 +21,25 @@ public class DataEnroller implements IDataEnroller {
 
 	protected Map<Class<? extends ITestData>, DataCollector<? extends ITestData>> collectors = new HashMap<>();
 	
-	public DataEnroller(RunNotificationSubscriber runNotificationSubscriber) {
+	protected DataEnroller() {
+		// TODO public of protected?
+	}
+	
+	private void configure(
+			RunNotificationSubscriber runNotificationSubscriber, Collection<String> sourceClassNames, 
+			Description topLevelDescription, File testDirectory, File codeDirectory) {
 		collectors.put(TestFailure.class, new TestFailureCollector(runNotificationSubscriber));
-		// TODO: andere default collectors toevoegen
+		collectors.put(MethodCalls.class, new TestDependencyCollector(sourceClassNames, runNotificationSubscriber));
+		collectors.put(CodeChange.class, new CodeChangeCollector(topLevelDescription, testDirectory, codeDirectory));
+	}
+	
+	public static DataEnroller createConfiguredDataEnroller(
+			RunNotificationSubscriber runNotificationSubscriber, Collection<String> sourceClassNames, 
+			Description topLevelDescription, File testDirectory, File codeDirectory) {
+		// TODO put all these arguments inside a value object (i.e. TestCollectionInfo)
+		DataEnroller dataEnroller = new DataEnroller();
+		dataEnroller.configure(runNotificationSubscriber, sourceClassNames, topLevelDescription, testDirectory, codeDirectory);
+		return dataEnroller;
 	}
 
 	@Override
